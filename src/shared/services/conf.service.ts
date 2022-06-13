6ab05892ccf0eb15da208a3919ead69c3a2215a5
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-06-13 09:00:36 -0500

    fix args

--- src/shared/services/conf.service.ts
+++ src/shared/services/conf.service.ts
@@ -21,20 +21,24 @@ export class ConfService extends ConfigService {
     });
 
     const args = parser.parse_args();
+    Object.keys(args).forEach(key => {
+      if (args[key] === undefined) args[key] = '';
+    });
 
     process.env.SSV_TOKEN_ADDRESS = this.SSV_TOKEN_ADDRESS;
     process.env.SSV_NETWORK_ADDRESS =
       args['contract_address'] || this.SSV_NETWORK_ADDRESS;
     process.env.GAS_PRICE = args['gas_price'] || this.GAS_PRICE;
-    process.env.ACCOUNT_PRIVATE_KEY = args['private_key'];
-    process.env.NODE_URL = args['node_url'];
+    process.env.ACCOUNT_PRIVATE_KEY =
+      args['private_key'] || process.env.ACCOUNT_PRIVATE_KEY;
+    process.env.NODE_URL = args['node_url'] || process.env.NODE_URL;
 
     let notFoundParam = null;
-    if (!!process.env.NODE_URL) {
+    if (!process.env.NODE_URL) {
       notFoundParam = 'NODE_URL';
-    } else if (!!process.env.ACCOUNT_PRIVATE_KEY) {
+    } else if (!process.env.ACCOUNT_PRIVATE_KEY) {
       notFoundParam = 'ACCOUNT_PRIVATE_KEY';
-    } else if (!!process.env.SSV_TOKEN_ADDRESS) {
+    } else if (!process.env.SSV_TOKEN_ADDRESS) {
       notFoundParam = 'SSV_TOKEN_ADDRESS';
     }
 
