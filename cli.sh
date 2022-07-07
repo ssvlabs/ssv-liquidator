79d83d3d094fde8cb9ee77ec2cfb7d7390d2290f
Dmitri Meshin <dmitri@blox.io>
2022-07-07 11:58:02 +0300

    Fix bash script

--- cli.sh
+++ cli.sh
@@ -2,11 +2,10 @@
 
 i=1
 RET=-1
-while [ $i -le 5 ] && [ $RET -ne 0 ]; do
-    [ $i -eq 1 ] || sleep 5
-    ./node_modules/.bin/ts-node -r tsconfig-paths/register src/services/worker/worker.tsx
+while [ $RET -ne 0 ] && [ $RET -ne 2 ]; do
+    [ $i -eq 1 ] || (sleep 5 && echo "Running Liquidator. Attempt #"$i)
+    ./node_modules/.bin/ts-node -r tsconfig-paths/register src/services/worker/worker.tsx $*
     RET=$?
     i=$((i+1))
-    echo "Running Liquidator. Attempt #"$i
 done
 exit $RET
